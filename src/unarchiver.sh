7eaa353a162d55ed614a5dca4e7191e4c68e7e19
x7bit <rayco@DESKTOP-QI52I1J.localdomain>
2020-05-29 18:02:50 +0100

    Bugfixing

--- src/unarchiver.sh
+++ src/unarchiver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 readKey() {
-    echo
+    echo;echo
     printf '\e[1;36m%-6s\e[m' "Press START to continue..."
     oldstty=`stty -g`
     stty -icanon -echo min 1 time 0
@@ -18,9 +18,11 @@ checkCmd() {
 }
 unarchive() {
     if $2; then
+        echo
         printf '\e[0;32m%-6s\e[m' "$(basename "$1"): successfully decompressed"
         sleep 0.4
     else
+        echo
         printf '\e[0;31m%-6s\e[m' "$(basename "$1"): decompression failed"
         readKey
     fi
@@ -30,10 +32,10 @@ ungzip() {
     totalSize=$(expr length "$1")
     if [ "$regexSize" = "$totalSize" ]; then
         checkCmd tar
-        unarchive "$1" $(tar -xzf "$1" -C "$dir" --overwrite)
+        unarchive "$1" "tar -xzf $1 -C $dir --overwrite"
     else
         checkCmd gunzip
-        unarchive "$1" $(gunzip -f "$1")
+        unarchive "$1" "gunzip -f $1"
     fi
 }
 byExtension() {
@@ -41,22 +43,22 @@ byExtension() {
     case $ext in
         "zip")
             checkCmd unzip
-            unarchive "$1" $(unzip -od "$dir" "$1")
+            unarchive "$1" "unzip -od $dir $1"
             ;;
         "gz")
             ungzip "$1"
             ;;
         "tar")
             checkCmd tar
-            unarchive "$1" $(tar -xf "$1" -C "$dir" --overwrite)
+            unarchive "$1" "tar -xf $1 -C $dir --overwrite"
             ;;
         "rar")
             checkCmd unrar
-            unarchive "$1" $(unrar x -o+ "$1" "$dir")
+            unarchive "$1" "unrar x -o+ $1 $dir"
             ;;
         "7z")
             checkCmd 7zr
-            unarchive "$1" $(7zr x -aoa -o"$dir" "$1")
+            unarchive "$1" "7zr x -aoa -o$dir $1"
             ;;
         *)
             printf '\e[0;31m%-6s\e[m' "$(basename "$1"): extension not recognized"
@@ -70,14 +72,14 @@ mime=$(file -bi "$1" | cut -d';' -f1)
 case $mime in
     "application/zip" | "application/x-zip-compressed")
         checkCmd unzip
-        unarchive "$1" $(unzip -od "$dir" "$1")
+        unarchive "$1" "unzip -od $dir $1"
         ;;
     "application/gzip" | "application/x-gzip")
         ungzip "$1"
         ;;
     "application/tar" | "application/x-tar")
         checkCmd tar
-        unarchive "$1" $(tar -xf "$1" -C "$dir" --overwrite)
+        unarchive "$1" "tar -xf $1 -C $dir --overwrite"
         ;;
     "application/tar+gzip")
         checkCmd tar
@@ -85,11 +87,11 @@ case $mime in
         ;;
     "application/x-rar" | "application/x-rar-compressed")
         checkCmd unrar
-        unarchive "$1" $(unrar x -o+ "$1" "$dir")
+        unarchive "$1" "unrar x -o+ $1 $dir"
         ;;
     "application/x-7z-compressed")
         checkCmd 7zr
-        unarchive "$1" $(7zr x -aoa -o"$dir" "$1")
+        unarchive "$1" "7zr x -aoa -o$dir $1"
         ;;
     "application/octet-stream")
         byExtension "$1"
