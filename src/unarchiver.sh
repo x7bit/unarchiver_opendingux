b8993e550aaa8d60233a0c7275b5d997309ad0b6
x7bit <rayco@DESKTOP-QI52I1J.localdomain>
2020-05-29 03:46:00 +0100

    Added .gz files. Check for extensions if MIME is octet-stream. Colors.

--- src/unarchiver.sh
+++ src/unarchiver.sh
@@ -1,25 +1,68 @@
 #!/bin/sh
 readKey() {
     echo
-    echo "Press START to continue..."
+    printf '\e[1;36m%-6s\e[m' "Press START to continue..."
     oldstty=`stty -g`
     stty -icanon -echo min 1 time 0
     dd bs=1 count=1 >/dev/null 2>&1
     stty "$oldstty"
-    echo
 }
 checkCmd() {
     which=$(which "$1")
     if [ -z "$which" ]; then
-        echo "$1: command not found"
+        printf '\e[0;31m%-6s\e[m' "$1: command not found"
         readKey
+        echo
         exit
     fi
 }
-noMime() {
-    echo "$1: MIME not recognized"
-    readKey
-    exit
+unarchive() {
+    if $2; then
+        printf '\e[0;32m%-6s\e[m' "$(basename "$1"): successfully decompressed"
+        sleep 0.6
+    else
+        printf '\e[0;31m%-6s\e[m' "$(basename "$1"): decompression failed"
+        readKey
+    fi
+}
+ungzip() {
+    regexSize=$(expr "$1" : '^.*.tar.gz$')
+    totalSize=$(expr length "$1")
+    if [ "$regexSize" = "$totalSize" ]; then
+        checkCmd tar
+        unarchive "$1" $(tar -xzf "$1" -C "$dir" --overwrite)
+    else
+        checkCmd gunzip
+        unarchive "$1" $(gunzip -f "$1")
+    fi
+}
+byExtension() {
+    ext=$(echo $1 | sed 's/^.*\.//')
+    case $ext in
+        "zip")
+            checkCmd unzip
+            unarchive "$1" $(unzip -od "$dir" "$1")
+            ;;
+        "gz")
+            ungzip "$1"
+            ;;
+        "tar")
+            checkCmd tar
+            unarchive "$1" $(tar -xf "$1" -C "$dir" --overwrite)
+            ;;
+        "rar")
+            checkCmd unrar
+            unarchive "$1" $(unrar x -o+ "$1" "$dir")
+            ;;
+        "7z")
+            checkCmd 7zr
+            unarchive "$1" $(7zr x -aoa -o"$dir" "$1")
+            ;;
+        *)
+            printf '\e[0;31m%-6s\e[m' "$(basename "$1"): extension not recognized"
+            readKey
+            ;;
+    esac
 }
 # MAIN BLOCK
 dir=$(dirname "$1")
@@ -27,26 +70,33 @@ mime=$(file -bi "$1" | cut -d';' -f1)
 case $mime in
     "application/zip")
         checkCmd unzip
-        unzip -od "$dir" "$1"
+        unarchive "$1" $(unzip -od "$dir" "$1")
+        ;;
+    "application/gzip")
+        ungzip "$1"
         ;;
     "application/tar")
         checkCmd tar
-        tar -xf "$1" -C "$dir" --overwrite
+        unarchive "$1" $(tar -xf "$1" -C "$dir" --overwrite)
         ;;
     "application/tar+gzip")
         checkCmd tar
-        tar -xzf "$1" -C "$dir" --overwrite
+        unarchive "$1" $(tar -xzf "$1" -C "$dir" --overwrite)
         ;;
     "application/x-rar")
         checkCmd unrar
-        unrar x -o+ "$1" "$dir"
+        unarchive "$1" $(unrar x -o+ "$1" "$dir")
         ;;
     "application/x-7z-compressed")
         checkCmd 7zr
-        7zr x -aoa -o"$dir" "$1"
+        unarchive "$1" $(7zr x -aoa -o"$dir" "$1")
+        ;;
+    "application/octet-stream")
+        byExtension "$1"
         ;;
     *)
-        noMime "$mime"
+        printf '\e[0;31m%-6s\e[m' "$mime: MIME not recognized"
+        readKey
         ;;
 esac
-usleep 400000
+echo
